314c50845ab79bf8a52e669aeff03737a4aac8e7
Lakshya Taneja <lakkutaneja@rediff.com>
2017-06-30 22:24:41 +0530

    Update xpe.mk

--- xpe.mk
+++ xpe.mk
@@ -2,6 +2,8 @@
 TARGET_SCREEN_WIDTH := 1080
 TARGET_SCREEN_HEIGHT := 1920
 
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
